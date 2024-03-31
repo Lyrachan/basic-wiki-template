075feae1783bd970f6f00a2619e99c53508f94d2
Jorge <jorge.liram9@gmail.com>
2024-03-31 01:26:35 -0300

    added validations for pages&articles for title and content

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :page
+
+  # Validations
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ApplicationRecord
     # Dependent destroy para borrado en cascada
     has_many :articles, dependent: :destroy
+
+    # Validations
+    validates :title, presence: true
+    validates :content, presence: true
 end
